41f03e8a3a66ba5a7befbe6b946f821443743ddb
Torjus Iveland <torjusti@gmail.com>
2017-10-18 09:48:28 +0200

    Add fixed point iteration for x = arccos(x)

--- o8.py
+++ o8.py
@@ -1,3 +1,19 @@
+import math
+
+def fixed_point_iteration(treshold=10e-4):
+    """Approximate the value of arccos(x)=x. Since a fixed
+    point of arccos(x)=x also is a fixed point of cos(x)=x,
+    we use cos(x)=x, as it has a derivative which ensures our
+    algorithm converges."""
+    value, previous = 1, 0
+
+    while abs(value - previous) >= treshold:
+        value, previous = math.cos(value), value
+
+    return value
+
+print(fixed_point_iteration())
+
 def quadratic(x):
     """The function we wish to find the roots of. This equation is
     just an alternative version of expressing the third root of 7."""
@@ -7,7 +23,7 @@ def derivative(x):
     """The derivative of the function we with to find the zeroes of."""
     return 3 * x ** 2
 
-def newtons_method(current=2, digits=3):
+def newtons_method(current=2, digits=4):
     """Run Newtons method with the provided initial guess
     and run enough iterations to get the provided amount
     of significant digits."""
