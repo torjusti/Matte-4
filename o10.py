ddc98181127e79f934c8cb96c318df65c0dcc838
Torjus Iveland <torjusti@gmail.com>
2017-10-30 15:44:45 +0100

    move divdif to o10

--- o10.py
+++ o10.py
@@ -1,3 +1,5 @@
+from fractions import Fraction
+
 def divided_difference(values):
   """Calculates the divided difference using the given samples."""
   if len(values) == 1:
@@ -8,4 +10,7 @@ def divided_difference(values):
 
   return (divided_difference(values[1:]) - divided_difference(values[0:-1])) / (values[-1][0] - values[0][0])
 
-print(divided_difference([(-2, -2), (-1, 1), (0, 2)]))
+tups = [(0, 4), (1, -1), (2, -3), (4, -6), (6, 9), (3, -5)]
+
+for i in range(0, len(tups)):
+  print(tups[:i+1], Fraction(divided_difference(tups[:i+1])).limit_denominator())
